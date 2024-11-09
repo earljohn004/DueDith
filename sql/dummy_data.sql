d8e27ea8c23802e475a35d45f2893831fcf6d22d
Earl John <earl.abaquita@gmail.com>
2024-11-09 20:06:50 +0800

    add sql query

---
+++ sql/dummy_data.sql
@@ -0,0 +1,141 @@
+INSERT INTO
+    public.profile (last_name, first_name, created_at)
+VALUES
+    ('Doe', 'John', NOW()),
+    ('Smith', 'Jane', NOW()),
+    ('Brown', 'Charlie', NOW()),
+    ('Johnson', 'Emily', NOW()),
+    ('Williams', 'Michael', NOW());
+
+-- Insert dummy data into the category table
+INSERT INTO
+    public.category (
+        profile_id,
+        NAME,
+        description,
+        TYPE,
+        created_at
+    )
+VALUES
+    (
+        501,
+        'Groceries',
+        'Monthly grocery expenses',
+        'EXPENSE',
+        NOW()
+    ),
+    (
+        501,
+        'Utilities',
+        'Monthly utility bills',
+        'EXPENSE',
+        NOW()
+    ),
+    (
+        502,
+        'Salary',
+        'Monthly salary income',
+        'INCOME',
+        NOW()
+    ),
+    (
+        502,
+        'Entertainment',
+        'Expenses for entertainment',
+        'EXPENSE',
+        NOW()
+    ),
+    (
+        503,
+        'Savings',
+        'Monthly savings',
+        'EXPENSE',
+        NOW()
+    );
+
+-- Insert dummy data into the budget table
+INSERT INTO
+    public.budget (
+        category_id,
+        profile_id,
+        start_date,
+        end_date,
+        INTERVAL,
+        created_at
+    )
+VALUES
+    (
+        (
+            SELECT
+                id
+            FROM
+                public.category
+            WHERE
+                NAME = 'Groceries'
+            LIMIT
+                1
+        ), 501, NOW(),
+        NOW() + INTERVAL '1 month',
+        1,
+        NOW()
+    ),
+    (
+        (
+            SELECT
+                id
+            FROM
+                public.category
+            WHERE
+                NAME = 'Utilities'
+            LIMIT
+                1
+        ), 501, NOW(),
+        NOW() + INTERVAL '1 month',
+        1,
+        NOW()
+    ),
+    (
+        (
+            SELECT
+                id
+            FROM
+                public.category
+            WHERE
+                NAME = 'Salary'
+            LIMIT
+                1
+        ), 502, NOW(),
+        NOW() + INTERVAL '1 month',
+        1,
+        NOW()
+    ),
+    (
+        (
+            SELECT
+                id
+            FROM
+                public.category
+            WHERE
+                NAME = 'Entertainment'
+            LIMIT
+                1
+        ), 502, NOW(),
+        NOW() + INTERVAL '1 month',
+        1,
+        NOW()
+    ),
+    (
+        (
+            SELECT
+                id
+            FROM
+                public.category
+            WHERE
+                NAME = 'Savings'
+            LIMIT
+                1
+        ), 503, NOW(),
+        NOW() + INTERVAL '1 month',
+        1,
+        NOW()
+    );
