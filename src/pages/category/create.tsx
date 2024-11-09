7f087bb60ca72a6b23e20f17f84c9677b218ec82
Earl John <earl.abaquita@gmail.com>
2024-11-09 21:12:34 +0800

    add create page

--- src/pages/category/create.tsx
+++ src/pages/category/create.tsx
@@ -1,14 +1,27 @@
-import { Box, TextField } from "@mui/material";
+import { Box, MenuItem, TextField } from "@mui/material";
+import { useSelect } from "@refinedev/core";
 import { Create } from "@refinedev/mui";
 import { useForm } from "@refinedev/react-hook-form";
+import { Controller } from "react-hook-form";
 
+interface Profile {
+  first_name: string;
+  last_name: string;
+  id: string;
+}
 export const CategoryCreate = () => {
   const {
     saveButtonProps,
     refineCore: { formLoading },
     register,
-    formState: { errors },
+    control,
   } = useForm({});
+
+  const { options } = useSelect<Profile>({
+    resource: "profile",
+    optionLabel: (item) => `${item.first_name} ${item.last_name}`,
+    optionValue: "id",
+  });
 
   return (
     <Create isLoading={formLoading} saveButtonProps={saveButtonProps}>
@@ -18,15 +31,59 @@ export const CategoryCreate = () => {
         autoComplete="off"
       >
         <TextField
-          {...register("title", {
+          {...register("name", {
             required: "This field is required",
           })}
           margin="normal"
           fullWidth
           InputLabelProps={{ shrink: true }}
           type="text"
-          label={"Title"}
-          name="title"
+          label={"Name"}
+          name="name"
+        />
+        <TextField
+          {...register("description", {
+            required: "This field is required",
+          })}
+          margin="normal"
+          fullWidth
+          InputLabelProps={{ shrink: true }}
+          type="text"
+          label={"Description"}
+          name="description"
+        />
+        <Controller
+          name="type"
+          control={control}
+          render={({ field }) => (
+            <TextField select label="type" {...field} margin="normal" fullWidth>
+              <MenuItem key={"INCOME"} value={"INCOME"}>
+                INCOME
+              </MenuItem>
+              <MenuItem key={"EXPENSE"} value={"EXPENSE"}>
+                EXPENSE
+              </MenuItem>
+            </TextField>
+          )}
+        />
+        <Controller
+          name="profile_id"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              select
+              label="Profile"
+              {...field}
+              margin="normal"
+              fullWidth
+            >
+              {options?.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          )}
         />
       </Box>
     </Create>
